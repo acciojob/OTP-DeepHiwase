3320c887859f9897e25dd7def9e85254277c130d
Deep Hiwase <114879645+DeepHiwase@users.noreply.github.com>
2026-01-05 15:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-//your JS code here. If required.
 // focus on first input ele when loaded
 const firstInput = document.querySelector(".code");
 firstInput.focus();
 
-const otpInputs = Array.from(document.querySelectorAll(".otp"));
+const otpInputs = Array.from(document.querySelectorAll(".code"));
 console.log(otpInputs)
 for (let otpInput of otpInputs) {
   // console.log(otpInput)
 
   otpInput.addEventListener("keyup", (e) => {
     if (e.key === "Backspace") {
-      const prevElement = otpInput.previousElementSibling;
-      prevElement.focus();
+      // otpInput.textContent = "";
+      while (otpInput.previousElementSibling && otpInput.previousElementSibling.textContent === "") {
+        const prevElement = otpInput.previousElementSibling;
+        prevElement.focus();
+        console.log("1");
+      }
       return;
     }
-    const nextElement = otpInput.nextElementSibling;
-    nextElement.focus();
+    if (e.key >= 0 && e.key <= 9) {
+      const nextElement = otpInput.nextElementSibling;
+      nextElement.focus();
+    } else {
+
+    }
   })
 
   // otpInput.addEventListener("keyup", (e) => {
